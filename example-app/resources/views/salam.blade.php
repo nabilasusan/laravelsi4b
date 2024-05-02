4faaedbbfe001c7bf5575662a72676b69a8a0224
nabilasusan <nabila_2226240101@mhs.mdp.ac.id>
2024-05-02 15:11:10 +0700

    fakultas

---
+++ example-app/resources/views/salam.blade.php
@@ -0,0 +1,2 @@
+<h1>Selamat {{ $viewsalam}} 
+<h1\>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,17 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('about', function(){
+    return "Halaman About";
+});
+
+Route::get('profil', function(){
+    return view('profil');
+
+});
+
+// Route dengan parameter
+Route::get('welcome/{salam}', function ($salam){
+    // return view('salam')
+    //return 'selamat '. $salam;
+    return view('salam')->with('viewsalam', $salam); 
+});
+
+// Route tanpa parameter
+// terdapat array $list
+Route::get('listdata', function(){
+    $list = ["Sistem Informasi", "Informatika","Manajemen"];
+    $listmhs = [
+        ["npm"=> 001, "nama"=>"ahmad"],
+        ["npm"=> 002, "nama"=>"budi"]
+    ];
+    return view('listprodi')
+         ->with('viewlist',$list)
+         ->with('viewmhs',$listmhs);
+
 });
