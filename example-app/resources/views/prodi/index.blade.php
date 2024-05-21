d3e1f43074a0e3249298bcd0aa169a16dc51835e
nabilasusan <nabila_2226240101@mhs.mdp.ac.id>
2024-05-21 17:01:31 +0700

    materiterbaru

--- example-app/resources/views/prodi/index.blade.php
+++ example-app/resources/views/prodi/index.blade.php
@@ -1,19 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>UMDP</h1>
-    <h2>program studi</h2>
-    <ul>
-        @foreach ($prodi as $item)
-    <li>{{ $item["nama"] }} {{ $item["singkatan"] }} 
-    {{$item["fakultas"]["nama"]}}</Ii>
-    @endforeach
-</ul>
-</body>
-</html>
+@extends('layout.main')
+
+@section('title','prodi')
+
+@section('content')
+
+  <div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Basic Table</h4>
+                  <p class="card-description">
+                    list data prodi
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>nama fakultas</th>
+                          <th>singkatan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach ($fakultas as $item)
+                         <tr>
+                            <td>{{ $item["nama"] }}</td>
+                            <td>{{ $item["singkatan"] }}</td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+@endsection
+
+
